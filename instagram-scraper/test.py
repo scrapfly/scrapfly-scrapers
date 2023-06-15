7b0f48bf36998b18e1218594fd608af3e8002b9a
granitosaurus <bernardas.alisauskas@pm.me>
2023-06-15 18:29:08 +0700

    add tests for instagram

---
+++ instagram-scraper/test.py
@@ -0,0 +1,60 @@
+from cerberus import Validator
+import pytest
+
+import instagram
+import pprint
+
+pp = pprint.PrettyPrinter(indent=2)
+
+# enable cache?
+instagram.BASE_CONFIG["cache"] = True
+
+
+def validate_or_fail(item, validator):
+    if not validator.validate(item):
+        pp.pformat(item)
+        pytest.fail(f"Validation failed for item: {pp.pformat(item)}\nErrors: {validator.errors}")
+
+
+@pytest.mark.asyncio
+async def test_user_scraping():
+    result = await instagram.scrape_user("google")
+    schema = {
+        "name": {"type": "string"},
+        "username": {"type": "string"},
+        "category": {"type": "string"},
+        "bio": {"type": "string"},
+        "followers": {"type": "integer"},
+        "follows": {"type": "integer"},
+    }
+    validator = Validator(schema, allow_unknown=True)
+    validate_or_fail(result, validator)
+
+
+@pytest.mark.asyncio
+async def test_post_scraping():
+    result = await instagram.scrape_post("https://www.instagram.com/p/Csthn7EO99u/")
+    schema = {
+        "id": {"type": "string"},
+        "shortcode": {"type": "string"},
+        "src": {"type": "string"},
+        "src_attached": {"type": "list", "schema": {"type": "string"}},
+        "likes": {"type": "integer"},
+        "taken_at": {"type": "integer"},
+        "comments_count": {"type": "integer"},
+        "captions": {"type": "list", "schema": {"type": "string"}},
+        "comments": {
+            "type": "list",
+            "schema": {
+                "type": "dict",
+                "schema": {
+                    "id": {"type": "string"},
+                    "text": {"type": "string"},
+                    "owner": {"type": "string"},
+                    "created_at": {"type": "integer"},
+                },
+            },
+        },
+    }
+    validator = Validator(schema, allow_unknown=True)
+    validate_or_fail(result, validator)
