0fdb7d0c293600f6fdedb857cc2fc487a616eecf
Mazen Ramadan <mazenramadan00@gmail.com>
2023-11-22 16:59:39 +0200

    add domain.com.au scraper

---
+++ domaincom-scraper/domaincom.py
@@ -0,0 +1,148 @@
+"""
+This is an example web scraper for domain.com.au
+
+To run this scraper set env variable $SCRAPFLY_KEY with your scrapfly API key:
+$ export $SCRAPFLY_KEY="your key from https://scrapfly.io/dashboard"
+"""
+import os
+import json
+import jmespath
+from scrapfly import ScrapeConfig, ScrapflyClient, ScrapeApiResponse
+from typing import Dict, List
+from pathlib import Path
+from loguru import logger as log
+
+
+SCRAPFLY = ScrapflyClient(key=os.environ["SCRAPFLY_KEY"])
+
+
+BASE_CONFIG = {
+    # bypass domain.com.au scraping blocking    
+    "asp": True,
+    # set the proxy country to australia
+    "country": "AU",
+}
+
+
+output = Path(__file__).parent / "results"
+output.mkdir(exist_ok=True)
+
+
+def parse_hidden_data(response: ScrapeApiResponse):
+    """parse json data from script tags"""
+    selector = response.selector
+    script = selector.xpath("//script[@id='__NEXT_DATA__']/text()").get()
+    data = json.loads(script)
+    with open("data.json", "w", encoding="utf-8") as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
+    return data["props"]["pageProps"]["componentProps"]
+
+
+def parse_property_page(data: Dict) -> Dict:
+    """refine property pages data"""
+    if not data:
+        return
+    result = jmespath.search(
+        """{
+    listingId: listingId,
+    listingUrl: listingUrl,
+    unitNumber: unitNumber,
+    streetNumber: streetNumber,
+    street: street,
+    suburb: suburb,
+    postcode: postcode,
+    createdOn: createdOn,
+    propertyType: propertyType,
+    beds: beds,
+    phone: phone,
+    agencyName: agencyName,
+    propertyDeveloperName: propertyDeveloperName,
+    agencyProfileUrl: agencyProfileUrl,
+    propertyDeveloperUrl: propertyDeveloperUrl,
+    description: description,
+    loanfinder: loanfinder,
+    schools: schoolCatchment.schools,
+    suburbInsights: suburbInsights,
+    gallery: gallery,
+    listingSummary: listingSummary,
+    agents: agents,
+    features: features,
+    structuredFeatures: structuredFeatures,
+    faqs: faqs
+    }""",
+        data,
+    )
+    return result
+
+
+def parse_search_page(data):
+    """refine search pages data"""
+    if not data:
+        return    
+    data = data["listingsMap"]
+    result = []
+    # iterate over card items in the search data
+    for key in data.keys():
+        item = data[key]
+        parsed_data = jmespath.search(
+            """{
+        id: id,
+        listingType: listingType,
+        listingModel: listingModel
+      }""",
+        item,
+        )
+        # execulde the skeletonImages key from the data
+        parsed_data["listingModel"].pop("skeletonImages")
+        result.append(parsed_data)
+    return result
+
+
+async def scrape_properties(urls: List[str]) -> List[Dict]:
+    """scrape listing data from property pages"""
+    # add the property page URLs to a scraping list
+    to_scrape = [ScrapeConfig(url, **BASE_CONFIG) for url in urls]
+    properties = []
+    # scrape all the property page concurrently
+    async for response in SCRAPFLY.concurrent_scrape(to_scrape):
+        # parse the data from script tag
+        data = parse_hidden_data(response)
+        # aappend the data to the list after refining
+        properties.append(parse_property_page(data))
+    log.success(f"scraped {len(properties)} property listings")
+    return properties
+
+
+async def scrape_search(url: str, max_scrape_pages: int = None):
+    """scrape property listings from search pages"""
+    first_page = await SCRAPFLY.async_scrape(ScrapeConfig(url, **BASE_CONFIG))
+    log.info("scraping search page {}", url)
+    data = parse_hidden_data(first_page)
+    search_data = parse_search_page(data)
+    # get the number of maximum search pages
+    max_search_pages = data["totalPages"]
+    # scrape all available pages if not max_scrape_pages or max_scrape_pages >= max_search_pages
+    if max_scrape_pages and max_scrape_pages < max_search_pages:
+        max_scrape_pages = max_scrape_pages
+    else:
+        max_scrape_pages = max_search_pages
+    log.info(
+        f"scraping search pagination, remaining ({max_scrape_pages - 1} more pages)"
+    )
+    # add the remaining search pages to a scraping list
+    other_pages = [
+        ScrapeConfig(
+            # paginate the search pages by adding a "?page" parameter at the end of the URL
+            str(first_page.context["url"]) + f"?page={page}",
+            **BASE_CONFIG,
+        )
+        for page in range(2, max_scrape_pages + 1)
+    ]
+    # scrape the remaining search pages concurrently
+    async for response in SCRAPFLY.concurrent_scrape(other_pages):
+        # parse the data from script tag        
+        data = parse_hidden_data(response)
+        # aappend the data to the list after refining        
+        search_data.extend(parse_search_page(data))
+    log.success(f"scraped ({len(search_data)}) from {url}")
+    return search_data
---
+++ domaincom-scraper/run.py
@@ -0,0 +1,42 @@
+"""
+This example run script shows how to run the domain.com.au scraper defined in ./domaincom.py
+It scrapes ads data and saves it to ./results/
+
+To run this script set the env variable $SCRAPFLY_KEY with your scrapfly API key:
+$ export $SCRAPFLY_KEY="your key from https://scrapfly.io/dashboard"
+"""
+import asyncio
+import json
+import domaincom
+from pathlib import Path
+
+output = Path(__file__).parent / "results"
+output.mkdir(exist_ok=True)
+
+
+async def run():
+    # enable scrapfly cache for basic use
+    domaincom.BASE_CONFIG["cache"] = True
+
+    print("running Domain.com.au scrape and saving results to ./results directory")
+
+    properties_data = await domaincom.scrape_properties(
+        urls = [
+            "https://www.domain.com.au/610-399-bourke-street-melbourne-vic-3000-2018835548",
+            "https://www.domain.com.au/404-258-flinders-lane-melbourne-vic-3000-2018819448",
+            "https://www.domain.com.au/101-29-31-market-street-melbourne-vic-3000-2018799963"
+        ]
+    )
+    with open(output.joinpath("properties.json"), "w", encoding="utf-8") as file:
+        json.dump(properties_data, file, indent=2, ensure_ascii=False)
+
+    search_data = await domaincom.scrape_search(
+        # you can change "sale" to "rent" in the search URL to search for properties for rent
+        url="https://www.domain.com.au/sale/melbourne-vic-3000/", max_scrape_pages=1
+    )
+    with open(output.joinpath("search.json"), "w", encoding="utf-8") as file:
+        json.dump(search_data, file, indent=2, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    asyncio.run(run())
