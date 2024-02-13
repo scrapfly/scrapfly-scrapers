bfb1a960a78fb112b7902cf35d74b4d286cec070
Mazen Ramadan <mazenramadan00@gmail.com>
2024-02-13 12:23:40 +0200

    scrape trustpilot reviews dunamically

--- trustpilot-scraper/trustpilot.py
+++ trustpilot-scraper/trustpilot.py
@@ -79,11 +79,25 @@ async def scrape_search(url: str, max_pages: int = None) -> List[Dict]:
     return search_data
 
 
+async def get_reviews_api_url(url: str) -> str:
+    """scrape the API version from the HTML and create the reviews API"""
+    response = await SCRAPFLY.async_scrape(
+        ScrapeConfig(url, **BASE_CONFIG)
+    )
+    buildId = json.loads(response.selector.xpath("//script[@id='__NEXT_DATA__']/text()").get())["buildId"]
+    business_unit = url.split("review/")[-1]
+
+    return f"https://www.trustpilot.com/_next/data/{buildId}/review/{business_unit}.json?sort=recency&businessUnit={business_unit}"
+
+
 async def scrape_reviews(url: str, max_pages: int = None) -> List[Dict]:
     """parse review data from the API"""
+    # create the reviews API url
+    log.info(f"getting the reviews API for the URL {url}")
+    api_url = await get_reviews_api_url(url)
     # send a POST request to the first review page and get the result directly in JSON
     first_page = await SCRAPFLY.async_scrape(
-        ScrapeConfig(url, method="POST", **BASE_CONFIG)
+        ScrapeConfig(api_url, method="POST", **BASE_CONFIG)
     )
     data = json.loads(first_page.scrape_result["content"])["pageProps"]
     reviews_data = data["reviews"]
@@ -96,7 +110,7 @@ async def scrape_reviews(url: str, max_pages: int = None) -> List[Dict]:
     log.info(f"scraping reviews pagination ({total_pages - 1} more pages)")
     # add the remaining search pages in a scraping list
     other_pages = [
-        ScrapeConfig(url + f"&page={page_number}", method="POST", **BASE_CONFIG)
+        ScrapeConfig(api_url + f"&page={page_number}", method="POST", **BASE_CONFIG)
         for page_number in range(2, total_pages + 1)
     ]
     # scrape the remaining search pages concurrently
