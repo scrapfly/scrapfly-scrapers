de0feb690947a7233cc139d974d234dd2effff20
md-scrapfly <mohamed@scrapfly.io>
2025-08-15 20:23:55 +0200

    [homegate] update outdated links

--- homegate-scraper/run.py
+++ homegate-scraper/run.py
@@ -23,8 +23,8 @@ async def run():
     properties_data = await homegate.scrape_properties(
         urls=[
             "https://www.homegate.ch/rent/4002086534",
-            "https://www.homegate.ch/rent/4002244507",
-            "https://www.homegate.ch/rent/4002268715"
+            "https://www.homegate.ch/rent/4002344585",
+            "https://www.homegate.ch/rent/4002421322",
         ]
     )
     with open(output.joinpath("properties.json"), "w", encoding="utf-8") as file:
